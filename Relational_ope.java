bb40e0dd05df98b2471414889006cbfe0d23e05c
kavya12378 <01fe23bca142@gmail.com>
2025-04-11 03:52:45 +0530

    save

---
+++ Relational_ope.java
@@ -0,0 +1,13 @@
+package Java_Program;
+
+public class Relational_ope {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+    System.out.println("a>b =" +(a>b));
+    System.out.println("a<b =" +(a<b));
+    System.out.println("a>=b =" +(a>=b));
+    System.out.println("a<=b =" +(a<=b));
+    System.out.println("a!=b =" +(a!=b));
+    }
+}
