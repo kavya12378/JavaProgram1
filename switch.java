ede96336ff1e58109e8863e432f7885e3e1601f0
kavya12378 <01fe23bca142@gmail.com>
2025-04-11 04:19:45 +0530

    save

---
+++ switch.java
@@ -0,0 +1,24 @@
+package Java_Program;
+
+public class switch {
+    public static void main(String[] args){
+        int x = 50;
+        int y = 20;
+        system.out.println("Enter the choice");
+        switch(ch)  {
+            case 1:Addition;
+            system.out.println("Addition =" +(x+y));
+            break;
+            case 2:Subtraction;
+            system.out.println("Subtraction =" +(x-y));
+            break;
+            case 3:Multiplication;
+            system.out.println("Multiplication =" +(x*y));
+            break;
+            case 4:Division;
+            system.out.println("Division =" +(x/y));
+            break;
+            default:System.out.println("Invalid choice");
+        }
+    }
+}
