79c1bb2541a49266fd4fd27be065a235e9ed5ba4
kavya12378 <01fe23bca142@gmail.com>
2025-04-11 04:33:53 +0530

    save

---
+++ class_method.java
@@ -0,0 +1,22 @@
+package Java_Program;
+
+public class class_method {
+    String Name;
+    int Age;
+    String SRN;
+    public static void main(String[] args) {
+        Name = n;
+        Age = age;
+        SRN = srn;
+    }
+    public void display()
+    {
+        System.out.println("Name: "+n);
+        System.out.println("Age: "+age);
+        System.out.println("SRN: "+srn);
+    }
+    public static void main(String[] args) {
+        student s = new student("kavya",19,"142");
+        s.display();
+    } 
+}
