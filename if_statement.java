37eb33e8658f299b9ce06331eacaef0b3ff0abbf
kavya12378 <01fe23bca142@gmail.com>
2025-04-11 04:12:03 +0530

    save

---
+++ if_statement.java
@@ -0,0 +1,23 @@
+package Java_Program;
+
+public class if_statement {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        if(x>0) {
+            System.out.println("x+y =" + (x+y));
+        }
+        if(x>0) {
+            System.out.println("x-y =" + (x-y));
+        }
+        if(x>0) {
+            System.out.println("x*y =" + (x*y));
+        }
+        if(x>0) {
+            System.out.println("x/y =" + (x/y));
+        }
+        if(x>0) {
+            System.out.println("x%y =" + (x%y));
+        }
+    }
+}
