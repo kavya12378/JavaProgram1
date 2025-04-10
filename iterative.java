01c790de4972aa548c28c4a7b82d1093f2eb5a86
kavya12378 <01fe23bca142@gmail.com>
2025-04-11 04:52:26 +0530

    save

---
+++ iterative.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class iterative {
+    public static void main(String[] args) {
+        int x, y ,z=0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter operand1");
+        x = scan.nextInt();
+        System.out.println("Enter operand2");
+        y = scan.nextInt();
+        System.out.println("Enter operator which u want(*,-,+,/)");
+    }
+}
